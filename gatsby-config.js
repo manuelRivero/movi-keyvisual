a36a219f5cc39a72e2f8db7442160900e5a19499
Manuel Rivero <Manuel.enrique.r.v@gmail.com>
2022-04-18 16:18:05 -0300

    [fix] gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,23 +9,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-VHCL68SLVP", // Google Analytics / GA
-          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        ],
-
-        // This object is used for configuration specific to this plugin
+        trackingIds: ["G-VHCL68SLVP"],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-         
+        },
       },
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-postcss",
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
